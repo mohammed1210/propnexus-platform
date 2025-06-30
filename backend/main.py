3c652f6fd1ad69373d3b70de6b532676d71dfcd2
mohammed1210 <58249364+mohammed1210@users.noreply.github.com>
2025-06-30 19:56:08 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,15 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from scraper.zoopla_scraper import scrape_zoopla_properties
 from scraper.rightmove_scraper import scrape_rightmove_properties
+import os
+from supabase import create_client, Client
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 app = FastAPI()
 
@@ -32,3 +41,8 @@ async def scrape_zoopla():
 async def scrape_rightmove():
     data = await scrape_rightmove_properties()
     return {"status": f"Rightmove scrape completed and {len(data)} properties fetched", "data": data}
+
+@app.get("/properties")
+async def get_properties():
+    response = supabase.table("properties").select("*").execute()
+    return response.data
