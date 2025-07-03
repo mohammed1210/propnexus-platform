8c3a2dae9792b8e1c446af9848a1d0366b8f696a
mohammed1210 <58249364+mohammed1210@users.noreply.github.com>
2025-07-03 21:08:58 +0100

    Update index.tsx

--- frontend/pages/properties/index.tsx
+++ frontend/pages/properties/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import Propertycard from "@/components/Propertycard";
+import Propertycard from "../../components/Propertycard";
 
 interface Property {
   id: string;
