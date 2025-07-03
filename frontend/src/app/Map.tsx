aa008a6f2ff4f408c89ef6036ae32478059624a5
mohammed1210 <58249364+mohammed1210@users.noreply.github.com>
2025-07-03 08:31:53 +0100

    Update Map.tsx

--- frontend/src/app/Map.tsx
+++ frontend/src/app/Map.tsx
@@ -1,31 +1,38 @@
 "use client";
 
-import { Property } from "./types";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import { Property } from "./types";
 import L from "leaflet";
+
+// Fix default marker icon (optional if default is missing)
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
+  iconRetinaUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
+});
 
 type MapProps = {
   properties: Property[];
 };
 
 export default function Map({ properties }: MapProps) {
-  const defaultPosition: [number, number] = [54.5, -3]; // Center of UK
-
   return (
-    <MapContainer center={defaultPosition} zoom={6} style={{ height: "600px", width: "100%" }}>
+    <MapContainer center={[54.5, -3]} zoom={6} style={{ height: "600px", width: "100%" }}>
       <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
+
       {properties
         .filter((p) => p.latitude && p.longitude)
         .map((p) => (
-          <Marker key={p.id} position={[p.latitude!, p.longitude!]} icon={L.icon({ iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png", iconSize: [25, 41], iconAnchor: [12, 41] })}>
+          <Marker key={p.id} position={[p.latitude, p.longitude]}>
             <Popup>
               <strong>{p.title}</strong><br />
               {p.address}<br />
-              £{p.price}
+              £{p.price.toLocaleString()}
             </Popup>
           </Marker>
         ))}
