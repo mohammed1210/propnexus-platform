6639812302465bf5fd8881671a5585fed16dbf1e
mohammed1210 <58249364+mohammed1210@users.noreply.github.com>
2025-07-13 19:12:07 +0100

    Update MapInner.tsx

--- frontend/src/app/MapInner.tsx
+++ frontend/src/app/MapInner.tsx
@@ -5,12 +5,11 @@ import type { Property } from "./types";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 
-// Fix marker icon default so it shows properly
-delete L.Icon.Default.prototype._getIconUrl;
+// Tell TypeScript to allow access to private
+delete (L.Icon.Default.prototype as any)._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
   iconUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
-  shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
 });
 
 interface MapProps {
