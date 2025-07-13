eafebb0d0ff3607541b478b9cff38cc3dde94150
mohammed1210 <58249364+mohammed1210@users.noreply.github.com>
2025-07-13 17:06:32 +0100

    Update Map.tsx

--- frontend/src/app/Map.tsx
+++ frontend/src/app/Map.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import type { Property } from "../types";
+import type { Property } from "./types";
 import "leaflet/dist/leaflet.css";
 
 interface MapProps {
