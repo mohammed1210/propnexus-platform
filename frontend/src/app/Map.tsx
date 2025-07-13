1fc1c6a42229d42644584ac66aaba02fd1ebfacc
mohammed1210 <58249364+mohammed1210@users.noreply.github.com>
2025-07-13 17:04:42 +0100

    Update Map.tsx

--- frontend/src/app/Map.tsx
+++ frontend/src/app/Map.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import type { Property } from "@/app/types";
+import type { Property } from "../types";
 import "leaflet/dist/leaflet.css";
 
 interface MapProps {
@@ -9,15 +9,19 @@ interface MapProps {
 
 export default function Map({ properties }: MapProps) {
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={6} style={{ height: "400px", width: "100%", marginTop: "2rem" }}>
+    <MapContainer center={[51.505, -0.09]} zoom={6} style={{ height: "500px", width: "100%" }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution="&copy; OpenStreetMap contributors"
       />
       {properties.map((property) => (
-        <Marker key={property.id} position={[property.latitude ?? 51.505, property.longitude ?? -0.09]}>
+        <Marker
+          key={property.id}
+          position={[property.latitude || 51.505, property.longitude || -0.09]}
+        >
           <Popup>
-            {property.title}<br />£{property.price.toLocaleString()}
+            <strong>{property.title}</strong><br />
+            £{property.price.toLocaleString()}
           </Popup>
         </Marker>
       ))}
