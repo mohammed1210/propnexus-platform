bf23f38a43a7e97f339b989bc57dc13daa3d4a63
mohammed1210 <58249364+mohammed1210@users.noreply.github.com>
2025-07-13 17:29:08 +0100

    Update Map.tsx

--- frontend/src/app/Map.tsx
+++ frontend/src/app/Map.tsx
@@ -1,30 +1,34 @@
 "use client";
+
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import type { Property } from "./types";
+import type { Property } from "../types";
 import "leaflet/dist/leaflet.css";
 
 interface MapProps {
   properties: Property[];
 }
 
-export default function Map({ properties }: MapProps) {
+export default function MapView({ properties }: MapProps) {
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={6} style={{ height: "500px", width: "100%" }}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution="&copy; OpenStreetMap contributors"
-      />
-      {properties.map((property) => (
-        <Marker
-          key={property.id}
-          position={[property.latitude || 51.505, property.longitude || -0.09]}
-        >
-          <Popup>
-            <strong>{property.title}</strong><br />
-            £{property.price.toLocaleString()}
-          </Popup>
-        </Marker>
-      ))}
-    </MapContainer>
+    <div style={{ height: "600px", width: "100%", marginTop: "2rem" }}>
+      <MapContainer center={[51.505, -0.09]} zoom={6} style={{ height: "100%", width: "100%" }}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {properties.map((property) => (
+          <Marker
+            key={property.id}
+            position={[property.latitude ?? 51.505, property.longitude ?? -0.09]}
+          >
+            <Popup>
+              <strong>{property.title}</strong><br />
+              £{property.price.toLocaleString()}<br />
+              {property.bedrooms} beds
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+    </div>
   );
 }
