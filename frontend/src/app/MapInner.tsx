dd602dcd865626f92e8fadf56a2f15ec0e22fe1e
mohammed1210 <58249364+mohammed1210@users.noreply.github.com>
2025-07-14 23:09:33 +0100

    Update MapInner.tsx

--- frontend/src/app/MapInner.tsx
+++ frontend/src/app/MapInner.tsx
@@ -1,46 +1,57 @@
 "use client";
-
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import type { Property } from "./types";
+import type { Property } from "../types";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
-
-// Fix default marker icon
-delete (L.Icon.Default.prototype as any)._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
-  iconUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
-  shadowUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
-});
+import { useEffect } from "react";
 
 interface MapProps {
   properties: Property[];
 }
 
 export default function MapInner({ properties }: MapProps) {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      // Only run on client side
+      delete (L.Icon.Default.prototype as any)._getIconUrl;
+
+      L.Icon.Default.mergeOptions({
+        iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+        iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+        shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+      });
+    }
+  }, []);
+
+  if (properties.length === 0) {
+    return <p>No properties with location data found.</p>;
+  }
+
   return (
-    <MapContainer center={[53.5, -2]} zoom={6} style={{ height: "400px", width: "100%" }}>
+    <MapContainer
+      center={[51.505, -0.09]} // default center, you can change
+      zoom={6}
+      scrollWheelZoom={true}
+      style={{ height: "500px", width: "100%", marginBottom: "2rem" }}
+    >
       <TileLayer
-        attribution='&copy; OpenStreetMap contributors'
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {properties.map((prop) =>
-        prop.latitude && prop.longitude ? (
-          <Marker
-            key={prop.id}
-            position={[prop.latitude, prop.longitude]}
-          >
-            <Popup>
-              <strong>{prop.title}</strong>
-              <br />
-              £{prop.price.toLocaleString()}
-            </Popup>
-          </Marker>
-        ) : null
-      )}
+      {properties.map((property) => (
+        <Marker
+          key={property.id}
+          position={[property.latitude, property.longitude]}
+        >
+          <Popup>
+            <strong>{property.title}</strong>
+            <br />
+            {property.location}
+            <br />
+            £{property.price.toLocaleString()}
+          </Popup>
+        </Marker>
+      ))}
     </MapContainer>
   );
 }
