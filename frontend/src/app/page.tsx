7c9387860d610593d558a4e89166b2b8796f5cc2
mohammed1210 <58249364+mohammed1210@users.noreply.github.com>
2025-07-15 20:46:18 +0100

    Update page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { useEffect, useState } from "react";
 import PropertyCard from "../../components/PropertyCard";
 import MapInner from "./MapInner";
