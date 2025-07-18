e00ce17a3e5999fd5c36cd0e8c64fda345bf87aa
mohammed1210 <58249364+mohammed1210@users.noreply.github.com>
2025-07-18 19:40:13 +0100

    Update supabaseClient.ts

--- frontend/lib/supabaseClient.ts
+++ frontend/lib/supabaseClient.ts
@@ -1,8 +1,9 @@
 // frontend/lib/supabaseClient.ts
-import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs';
-import { Database } from '../types/supabase';
+'use client';
+import { createBrowserClient } from '@supabase/ssr';
+import { type Database } from '../types/supabase';
 
-export const supabase = createPagesBrowserClient<Database>({
-  supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-});
+export const supabase = createBrowserClient<Database>(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
