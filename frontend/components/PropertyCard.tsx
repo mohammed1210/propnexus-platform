0ac0fb7eb064bc48583df46abb2778ec1bd94b6c
mohammed1210 <58249364+mohammed1210@users.noreply.github.com>
2025-07-18 18:39:10 +0100

    Update PropertyCard.tsx

--- frontend/components/PropertyCard.tsx
+++ frontend/components/PropertyCard.tsx
@@ -4,7 +4,7 @@
 import React from 'react';
 import styles from './PropertyCard.module.css';
 import Link from 'next/link';
-import { supabase } from '../lib/supabaseClient';
++ import { supabase } from '@lib/supabaseClient';
 import { useSession } from '@supabase/auth-helpers-react';
 
 interface Property {
