88c4736217d01d6247de07b4d446096f72704d21
mohammed1210 <58249364+mohammed1210@users.noreply.github.com>
2025-07-23 13:54:30 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const nextConfig = {
     config.resolve.alias['@components'] = path.resolve(__dirname, 'frontend/components');
     config.resolve.alias['@details'] = path.resolve(__dirname, 'frontend/components/property_details');
     config.resolve.alias['@map'] = path.resolve(__dirname, 'frontend/src/app');
+    config.resolve.alias['@lib'] = path.resolve(__dirname, 'frontend/lib');
     return config;
   }
 };
