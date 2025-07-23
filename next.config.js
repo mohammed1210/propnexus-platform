196a7372c24969bc0746bd6096fbee9e7136de1a
mohammed1210 <58249364+mohammed1210@users.noreply.github.com>
2025-07-23 13:35:53 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
-/** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: false,
   webpack(config) {
-    config.resolve.alias['@'] = require('path').resolve(__dirname, 'frontend');
-    config.resolve.alias['@components'] = require('path').resolve(__dirname, 'frontend/components');
-    config.resolve.alias['@details'] = require('path').resolve(__dirname, 'frontend/components/property_details');
-    config.resolve.alias['@map'] = require('path').resolve(__dirname, 'frontend/src/app');
+    config.resolve.alias['@'] = path.resolve(__dirname, '.');
+    config.resolve.alias['@components'] = path.resolve(__dirname, 'components');
+    config.resolve.alias['@details'] = path.resolve(__dirname, 'components/property_details');
+    config.resolve.alias['@map'] = path.resolve(__dirname, 'app');
     return config;
   },
 };
