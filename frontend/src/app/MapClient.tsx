2ebab13473d7ecf0dfa252d2b44ccfa9675df2fc
mohammed1210 <58249364+mohammed1210@users.noreply.github.com>
2025-07-25 15:38:20 +0100

    Update MapClient.tsx

--- frontend/src/app/MapClient.tsx
+++ frontend/src/app/MapClient.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { Property } from '../../src/types';
+import { Property } from "@/types";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import { useEffect } from "react";
@@ -14,7 +14,8 @@ export default function MapClient({ properties }: MapProps) {
   useEffect(() => {
     delete (L.Icon.Default.prototype as any)._getIconUrl;
     L.Icon.Default.mergeOptions({
-      iconRetinaUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png",
+      iconRetinaUrl:
+        "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png",
       iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
       shadowUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
     });
@@ -31,11 +32,11 @@ export default function MapClient({ properties }: MapProps) {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       {properties
-        .filter((property) => property.latitude !== undefined && property.longitude !== undefined)
+        .filter((property) => property.latitude && property.longitude)
         .map((property) => (
           <Marker
             key={property.id}
-            position={[property.latitude as number, property.longitude as number]}
+            position={[property.latitude!, property.longitude!] as [number, number]}
           >
             <Popup>
               <strong>{property.title}</strong>
