eeda7bbd7df2fe1bf1d6b4b0d0f0e6fd3e8ce678
mohammed1210 <58249364+mohammed1210@users.noreply.github.com>
2025-08-08 14:03:26 +0100

    Update page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useMemo, useState } from 'react';
 import dynamic from 'next/dynamic';
 import PropertyCard from '../../components/PropertyCard';
-import { Property } from './types';
+import { Property } from '../types';
 
 const MapView = dynamic(() => import('./MapView'), { ssr: false });
 
