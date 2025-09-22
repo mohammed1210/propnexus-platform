18fed4d0fb431a99e7e5a627a2e07272f42691c2
mohammed1210 <58249364+mohammed1210@users.noreply.github.com>
2025-09-22 00:26:32 +0000

    saved-deals

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -4,7 +4,7 @@ import type { NextRequest } from 'next/server';
 export function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
 
-  if (pathname === "/saved-deals-deals') {
+  if (pathname === "/saved-deals') {
     return NextResponse.redirect(new URL("/saved-deals', req.url));
   }
   if (pathname === '/off-market-deals') {
