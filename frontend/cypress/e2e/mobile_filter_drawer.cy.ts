2876c1ad5f01d01ecfe293287c6b65bf2e311eef
Mohammed Abbas <58249364+mohammed1210@users.noreply.github.com>
2026-03-19 14:57:20 +0000

    test(cypress): harden mobile drawer assertions

--- frontend/cypress/e2e/mobile_filter_drawer.cy.ts
+++ frontend/cypress/e2e/mobile_filter_drawer.cy.ts
@@ -3,9 +3,26 @@ describe('Mobile filter drawer', () => {
     cy.viewport('iphone-6');
     cy.visit('/search?q=london');
 
-    cy.get("button[aria-controls='mobile-filter-drawer']").first().click({ force: true });
-    cy.contains('button', '3').click({ force: true });
-    cy.contains('button', /^Apply$/i).click({ force: true });
+    cy.get("button[aria-controls='mobile-filter-drawer']")
+      .filter(':visible')
+      .first()
+      .as('drawerToggle')
+      .should('have.attr', 'aria-expanded', 'false')
+      .click();
+
+    cy.get('#mobile-filter-drawer').should('have.class', 'translate-y-0');
+    cy.get('@drawerToggle').should('have.attr', 'aria-expanded', 'true');
+
+    cy.get('#mobile-filter-drawer').within(() => {
+      cy.contains('button', /^3$/).click();
+    });
     cy.location('search').should('include', 'beds=3-3');
+
+    cy.get('#mobile-filter-drawer').within(() => {
+      cy.contains('button', /^Apply$/i).click();
+    });
+
+    cy.get('#mobile-filter-drawer').should('have.class', 'translate-y-full');
+    cy.get('@drawerToggle').should('have.attr', 'aria-expanded', 'false');
   });
 });
