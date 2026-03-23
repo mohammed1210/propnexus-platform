033dee6a55594ec67d92ce0fd1704a95d31aaa42
Mohammed Abbas <58249364+mohammed1210@users.noreply.github.com>
2026-03-23 20:58:12 +0000

    test(cypress): stabilize mobile drawer spec execution

--- frontend/cypress/e2e/mobile_filter_drawer.cy.ts
+++ frontend/cypress/e2e/mobile_filter_drawer.cy.ts
@@ -3,26 +3,32 @@ describe('Mobile filter drawer', () => {
     cy.viewport('iphone-6');
     cy.visit('/search?q=london');
 
-    cy.get("button[aria-controls='mobile-filter-drawer']")
+    const drawerToggleSelector = "button[aria-controls='mobile-filter-drawer']";
+
+    cy.get(drawerToggleSelector).filter(':visible').first().should('have.attr', 'aria-expanded', 'false');
+    cy.get(drawerToggleSelector).filter(':visible').first().click({ force: true });
+    cy.get(drawerToggleSelector)
       .filter(':visible')
       .first()
-      .as('drawerToggle')
-      .should('have.attr', 'aria-expanded', 'false')
-      .click();
+      .then(($btn) => {
+        if ($btn.attr('aria-expanded') !== 'true') {
+          cy.wrap($btn).click({ force: true });
+        }
+      });
 
-    cy.get('#mobile-filter-drawer').should('have.class', 'translate-y-0');
-    cy.get('@drawerToggle').should('have.attr', 'aria-expanded', 'true');
+    cy.get(drawerToggleSelector).filter(':visible').first().should('have.attr', 'aria-expanded', 'true');
+    cy.get('#mobile-filter-drawer').should('not.have.class', 'translate-y-full');
 
     cy.get('#mobile-filter-drawer').within(() => {
-      cy.contains('button', /^3$/).click();
+      cy.contains('button', /^3$/).should('be.visible').click();
     });
     cy.location('search').should('include', 'beds=3-3');
 
     cy.get('#mobile-filter-drawer').within(() => {
-      cy.contains('button', /^Apply$/i).click();
+      cy.contains('button', /^Apply$/i).should('be.visible').click();
     });
 
     cy.get('#mobile-filter-drawer').should('have.class', 'translate-y-full');
-    cy.get('@drawerToggle').should('have.attr', 'aria-expanded', 'false');
+    cy.get(drawerToggleSelector).filter(':visible').first().should('have.attr', 'aria-expanded', 'false');
   });
 });
